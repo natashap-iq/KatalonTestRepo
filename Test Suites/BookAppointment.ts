8129851fad1f297b9ed62cc151fe863a35898ea1
npewtner <npewtner@CAW2200264.ca.deloitte.com>
2024-11-21 14:44:18 -0600

    New Book Appointment test

---
+++ Test Suites/BookAppointment.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>BookAppointment</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d0df3f38-0df8-432b-a150-66d8386f3f20</testSuiteGuid>
+   <testCaseLink>
+      <guid>552cba12-ae55-4954-81ac-e7a4495be258</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/BookAppointment</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
