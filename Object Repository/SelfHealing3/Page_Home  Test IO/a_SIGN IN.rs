163ba7ffc615585d933ab4dd4cdd9a63e1918bb1
npewtner <npewtner@CAW2200264.ca.deloitte.com>
2024-11-29 11:10:43 -0600

    Added selfhealing3 test suite

---
+++ Object Repository/SelfHealing3/Page_Home  Test IO/a_SIGN IN.rs
@@ -0,0 +1,154 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_SIGN IN</name>
+   <tag></tag>
+   <elementGuidId>748a0f57-3e07-4e67-861d-a39b7d9cde8c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='wrapper']/div[2]/div/header/div/div/div[2]/div/div/a</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a.cta-button__button.button-cta--sign-in</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;SIGN IN&quot;s]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>29136f8f-957b-4967-a97b-251c6c5e70c8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://app.test.io/</value>
+      <webElementGuid>56c16402-7f49-4117-94d6-6cde1d76e979</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>cta-button__button button-cta--sign-in</value>
+      <webElementGuid>d55ebeff-fb20-481a-b05a-868fa4fe62af</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>target</name>
+      <type>Main</type>
+      <value>_blank</value>
+      <webElementGuid>171a07fa-2af1-464c-9c19-b6e4b69a1699</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>SIGN IN</value>
+      <webElementGuid>e74ce3e5-e4c8-4422-9296-d9b79e99f654</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;wrapper&quot;)/div[@class=&quot;header-container iparsys parsys&quot;]/div[@class=&quot;header section&quot;]/header[@class=&quot;test-io header-ui&quot;]/div[@class=&quot;header__top&quot;]/div[@class=&quot;header__content top&quot;]/div[@class=&quot;header__container-right&quot;]/div[@class=&quot;cta-button&quot;]/div[@class=&quot;cta-button__container&quot;]/a[@class=&quot;cta-button__button button-cta--sign-in&quot;]</value>
+      <webElementGuid>1569e205-def6-49a4-8ebc-7e32d1823c94</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='wrapper']/div[2]/div/header/div/div/div[2]/div/div/a</value>
+      <webElementGuid>d154376c-b05d-454e-975c-2188f057022e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'SIGN IN')]</value>
+      <webElementGuid>ce67e0bb-1347-4f23-9a7f-f797ae7a9e26</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='TEST IO'])[1]/following::a[1]</value>
+      <webElementGuid>bbd2f38e-b57e-446b-b91f-4a29e5510325</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Skip navigation'])[1]/following::a[2]</value>
+      <webElementGuid>6034b68b-51fe-48e5-9436-2eff39dfc9a9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='MENU'])[1]/preceding::a[1]</value>
+      <webElementGuid>d57910cb-3b34-4c98-bfbf-34721927b2d1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Global (EN)'])[1]/preceding::a[1]</value>
+      <webElementGuid>e9925a6c-f66b-4481-8324-86337bdae54b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='SIGN IN']/parent::*</value>
+      <webElementGuid>2b9f7cb4-8c5a-4f09-8e30-2e1867efc9d8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, 'https://app.test.io/')]</value>
+      <webElementGuid>9b411cde-5509-43b1-95f0-5f6639e3a6b4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/a</value>
+      <webElementGuid>8a4fd21d-b8c8-4a34-b84c-bc5c0b482edb</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'https://app.test.io/' and (text() = 'SIGN IN' or . = 'SIGN IN')]</value>
+      <webElementGuid>62ef4190-5bb3-44f3-9eab-ccc3d5591e11</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/SelfHealing3.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SelfHealing3</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a79f20ad-bf17-4364-b115-1b1d5ff9de4a</testSuiteGuid>
+   <testCaseLink>
+      <guid>9aead817-b295-47a2-82fe-6caf2ba7dde1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SelfHealing3</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
